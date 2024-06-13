c07dddaab4248e358be117ad33d640061b638101
Gagik <gagiknaghashyan@gmail.com>
2024-06-13 17:30:47 +0400

    My first git commit

---
+++ Lesson1/Lesson1/src/Lesson1.java
@@ -0,0 +1,56 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+
+        // Lesson 1 of Java
+
+        System.out.print("Hello World!\n");
+        System.out.println("Lesson1");
+
+        // \n is doing enter next line
+
+        // Number types in Java
+
+        byte a = 127;
+        System.out.println(a);
+
+        short b = 4546;
+        System.out.println(b);
+
+        int x = 49838;
+        System.out.println(x);
+
+        long y = 2827192L;
+        System.out.println(y);
+
+        double d = 45.4;
+        System.out.println(d);
+        float f = 45.4f;
+        System.out.println(f);
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+        // even we can change variable value
+
+        char t = 'a';
+        System.out.println(t);
+        char tt = '\u0040';
+        System.out.println(tt);
+
+        String letterLine = "Hello from Java Code";
+        System.out.println(letterLine);
+
+        String firstName = "James";
+        String lastName = "Brown";
+
+        System.out.println(firstName + " " + lastName);
+
+        int k = 94;
+
+        System.out.println("k = " + k);
+
+        // + with about string that means concat this line
+    }
+}
