c275afa41e06a9287ee1d36fd2958e0cdcc5e593
GagikNaghashyan <gagikNaghashyan2@gmail.com>
2024-06-15 01:20:00 +0400

    boot first commit

---
+++ src/JavaMain.java
@@ -0,0 +1,67 @@
+public class JavaMain {
+    public static void main(String[] args) {
+
+        // Java Lesson 1 code data types
+
+        System.out.print("Hello World\n");
+        System.out.println("Hello Code Java");
+
+
+        // Numbers type
+
+        byte a = 127;
+        System.out.println(a);
+
+        short b = 4733;
+        System.out.println(b);
+
+        int x = 3737782;
+        System.out.println(x);
+
+        long y = 737822929L;
+        System.out.println(y);
+
+        // Numbers type with point
+
+        double aa = 43.88;
+        System.out.println(aa);
+
+        float bb = 33.85f;
+        System.out.println(bb);
+
+        // character type
+
+        char c = 'a';
+        System.out.println(c);
+
+        char cc = '\u003F';
+        System.out.println(cc);
+
+        /*
+        boolean type
+        true
+        false
+         */
+
+        boolean v = true;
+        System.out.println(v);
+        v = false;
+        System.out.println(v);
+
+        // String type
+
+        String letter = "Hello I'm letter";
+        System.out.println(letter);
+
+        String firstName = "James";
+        String lastName = "Brown";
+
+        // + that means in string concat
+
+        System.out.println(firstName + " " + lastName);
+
+        int k = 93;
+
+        System.out.println("k = " + k);
+    }
+}
